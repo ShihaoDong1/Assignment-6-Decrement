ba29d95295fddc308a6662490674c59b9d410d3e
ShihaoDong1 <shihao.dong1@gmail.com>
2020-06-03 08:46:02 -0400

    Updates

---
+++ precache-manifest.c4f3671b82d6c35c49ff73fce95134df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8817d2fd1c9cef4c1dc6c101467e7707",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "b529b5ce42664331e45c",
+    "url": "/repository-name/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7bb40fa3b82b5a168687",
+    "url": "/repository-name/static/js/2.2c7a8cfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository-name/static/js/2.2c7a8cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b529b5ce42664331e45c",
+    "url": "/repository-name/static/js/main.ecbe37c7.chunk.js"
+  },
+  {
+    "revision": "a50917693cef15d47834",
+    "url": "/repository-name/static/js/runtime-main.594016d5.js"
+  }
+]);
